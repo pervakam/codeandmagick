55dca772fe97fbddd73028e61b177f98c20b8628
pervakam <52745442+pervakam@users.noreply.github.com>
2020-02-06 15:04:13 +0300

    Учебный проект: холст

--- js/stat.js
+++ js/stat.js
@@ -4,12 +4,11 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
 
-
 var RESULT_X = 150;
-var RESULT_Y = 80;
-var RESULT_WIDTH = 40;
-var RESULT_HEIGHT = 150;
+var RESULT_Y = 230;
 var RESULT_GAP = 90;
+var RESULT_WIDTH = 40;
+var RESULT_HEIGHT = -150;
 
 var NAME_X = 150;
 var NAME_Y = 250;
@@ -21,33 +20,40 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var renderResult = function (ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, RESULT_WIDTH, RESULT_HEIGHT);
-};
-
 var findMaxElement = function(arr) {
   var maxElement = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  };
   return maxElement;
 };
 
-window.renderStatistics = function (ctx, players, times) {
+window.renderStatistics = function (ctx, players, time) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.fillStyle = NAME_COLOR;
   ctx.font = "16px PTMono";
-  ctx.fillText('Ура вы победили!', 250, 40);
-  ctx.fillText('Список результатов:', 245, 60);
+  ctx.fillText('Ура вы победили!', 250, 30);
+  ctx.fillText('Список результатов:', 245, 50);
 
-  // var players = ['Вы', 'Ниф-Ниф', 'Наф-Наф', 'Нуф-Нуф'];
+  var players = ['Вы', 'Ниф-Ниф', 'Наф-Наф', 'Нуф-Нуф'];
   var resultColor = ['#000', 'hsl(240, 20%, 50%)', 'hsl(240, 100%, 50%)', 'hsl(240, 40%, 50%)'];
+  var maxTime = findMaxElement(time);
+
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = NAME_COLOR;
     ctx.fillText(players[i], NAME_X + RESULT_GAP * i, NAME_Y);
-    renderResult(ctx, RESULT_X + RESULT_GAP * i, RESULT_Y, resultColor[i]);
-  };
 
-}
+    var resultBox = (RESULT_HEIGHT * time[i]) / maxTime;
+
+    ctx.fillStyle = resultColor[i];
+    ctx.fillRect(RESULT_X + RESULT_GAP * i, RESULT_Y, RESULT_WIDTH, resultBox);
+    ctx.fillStyle = NAME_COLOR;
+    ctx.fillText(Math.round(time[i]), NAME_X + RESULT_GAP * i, RESULT_Y + resultBox - 5);
+  };
+};
 
